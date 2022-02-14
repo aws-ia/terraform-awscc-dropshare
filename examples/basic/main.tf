50b06cf01ff5b7522cd399fe53958b5c165052af
Kerim Satirli <k.satirli@gmail.com>
2022-02-14 12:35:22 +0100

    adds documentation

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,11 @@
+# see https://www.terraform.io/language/modules/syntax#calling-a-child-module
 module "dropshare" {
+  # see https://www.terraform.io/language/modules/sources#local-paths
   source = "../.."
 
+  # CloudFront and ACM require resources to be created in a specific
+  # region, so a secondary AWS Provider is passed as `aws.certificates`
+  # see https://www.terraform.io/language/meta-arguments/module-providers
   providers = {
     aws              = aws
     aws.certificates = aws.certificates
