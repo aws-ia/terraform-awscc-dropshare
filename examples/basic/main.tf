62d88e86949e250bbde1a24c975848352816d0a3
Kerim Satirli <k.satirli@gmail.com>
2022-02-14 13:01:09 +0100

    linting

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,3 +16,29 @@ module "dropshare" {
   routed53_zone_id         = "Z022183919I9SRBSJMSTV"
   route53_record_subdomain = "operatehappy-dropshare-aws"
 }
+
+# see https://www.terraform.io/language/values/outputs
+output "dropshare_bucket_name" {
+  description = "S3 Bucket Name for Dropshare Connection."
+  value       = module.dropshare.bucket_name
+}
+
+# see https://www.terraform.io/language/values/outputs
+output "dropshare_access_key_id" {
+  description = "Access Key ID Name for Dropshare Connection."
+  value       = module.dropshare.iam_access_key
+}
+
+# update the `value` attronite of the `dropshare_secret_key_decrypt_command`
+# output if the identifier of the `dropshare_secret_key` output is changed
+# see https://www.terraform.io/language/values/outputs
+output "dropshare_secret_key" {
+  description = "Encrypted Secret Key for Dropshare Connection."
+  value       = module.dropshare.iam_secret_access_key
+}
+
+# see https://www.terraform.io/language/values/outputs
+output "dropshare_secret_key_decrypt_command" {
+  description = "Command to decrypt IAM Secret Access Key for Dropshare Connection."
+  value       = "${module.dropshare.decrypt_command_prepend} dropshare_secret_key ${module.dropshare.decrypt_command_append}"
+}
--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "iam_access_key" {
   description = "AWS IAM Access Key."
   value       = aws_iam_access_key.main.id
 }
+
+output "iam_secret_access_key" {
+  description = "AWS IAM (encrypted) Secret Access Key."
+  value       = aws_iam_access_key.main.encrypted_secret
+}
+
+output "decrypt_command_prepend" {
+  description = "Prepended part of Command to decrypt IAM Secret Access Key."
+  value       = "terraform output -raw"
+}
+
+output "decrypt_command_append" {
+  description = "Appended part of Command to decrypt IAM Secret Access Key."
+  value       = "| base64 --decode | keybase pgp decrypt"
+}
