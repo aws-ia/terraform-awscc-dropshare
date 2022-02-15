cdef4e20f36c454dbec9c74dadb68cf176abdd21
Kerim Satirli <k.satirli@gmail.com>
2022-02-15 15:29:29 +0100

    linting

---
+++ main.tf
@@ -0,0 +1,7 @@
+# This file intentionally left blank.
+#
+# For resources, see:
+#
+#  * `iam.tf`: IAM resources.
+#  * `networking.tf`: ACM, Route53, and CloudFront resources.
+#  * `storage.tf`: S3 resources.
