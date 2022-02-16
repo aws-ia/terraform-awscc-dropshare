6f12dbf202f030abaa3a0d7fed0924eadd9282ea
Kerim Satirli <k.satirli@gmail.com>
2022-02-16 15:34:40 +0100

    linting

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,13 +20,13 @@ module "dropshare" {
 }
 
 # see https://www.terraform.io/language/values/outputs
-output "dropshare_bucket_name" {
+output "bucket_name" {
   description = "S3 Bucket Name for Dropshare Connection."
   value       = module.dropshare.bucket_name
 }
 
 # see https://www.terraform.io/language/values/outputs
-output "dropshare_access_key_id" {
+output "access_key_id" {
   description = "Access Key ID Name for Dropshare Connection."
   value       = module.dropshare.access_key_id
 }
@@ -34,19 +34,25 @@ output "dropshare_access_key_id" {
 # update the `value` attronite of the `dropshare_secret_key_decrypt_command`
 # output if the identifier of the `dropshare_secret_key` output is changed
 # see https://www.terraform.io/language/values/outputs
-output "dropshare_secret_key" {
+output "secret_key" {
   description = "Encrypted Secret Key for Dropshare Connection."
   value       = module.dropshare.secret_key
 }
 
 # see https://www.terraform.io/language/values/outputs
-output "dropshare_secret_key_decrypt_command" {
+output "secret_key_decrypt_command" {
   description = "Command to decrypt IAM Secret Access Key for Dropshare Connection."
   value       = "${module.dropshare.secret_key_decrypt_command_prepend} dropshare_secret_key ${module.dropshare.secret_key_decrypt_command_append}"
 }
 
 # see https://www.terraform.io/language/values/outputs
-output "dropshare_region" {
+output "region" {
   description = "Region for Dropshare Connection."
   value       = module.dropshare.region
+}
+
+# see https://www.terraform.io/language/values/outputs
+output "domain_alias" {
+  description = "Domain Alias for Dropshare Connection."
+  value       = module.dropshare.domain_alias
 }
