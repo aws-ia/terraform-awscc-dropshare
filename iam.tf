00608ff4590e376cfb2aef660f3ec1ce25d1e9cc
Kerim Satirli <k.satirli@gmail.com>
2022-02-25 14:49:07 +0100

    linting

--- iam.tf
+++ iam.tf
@@ -12,9 +12,9 @@ resource "aws_iam_access_key" "main" {
 
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
 # also see https://learn.hashicorp.com/tutorials/terraform/aws-iam-policy#refactor-your-policy
-data "aws_iam_policy_document" "main" {
+data "aws_iam_policy_document" "user" {
   statement {
-    sid    = "AllowListOnBucket"
+    sid    = "AllowListOperationsOnBucket"
     effect = "Allow"
 
     actions = [
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "main" {
   }
 
   statement {
-    sid    = "AllowCRUDOnBucketObjects"
+    sid    = "AllowCrudOperationsOnBucketObjects"
     effect = "Allow"
 
     actions = [
@@ -66,7 +66,7 @@ data "aws_iam_policy_document" "main" {
 
   # see https://aws.amazon.com/premiumsupport/knowledge-center/s3-bucket-policy-for-config-rule/
   statement {
-    sid    = "DenyInsecureOperations"
+    sid    = "DenyInsecureOperationsOnBucketAndBucketObjects"
     effect = "Deny"
 
     actions = [
@@ -77,11 +77,6 @@ data "aws_iam_policy_document" "main" {
       aws_s3_bucket.main.arn,
       "${aws_s3_bucket.main.arn}/*"
     ]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
 
     # see https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_condition-keys.html#condition-keys-sourceip
     condition {
@@ -97,7 +92,7 @@ data "aws_iam_policy_document" "main" {
 
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user_policy
 resource "aws_iam_user_policy" "main" {
-  policy      = data.aws_iam_policy_document.main.json
+  policy      = data.aws_iam_policy_document.user.json
   name_prefix = "${aws_s3_bucket.main.id}-"
   user        = aws_iam_user.main.name
 }
